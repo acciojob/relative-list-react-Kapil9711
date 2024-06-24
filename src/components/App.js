05f38b20ae658fad8609bfb2e7874bf17f9c21fa
kapil <kapil.personal9711@gmail.com>
2024-06-24 22:31:15 +0530

    s

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,11 @@ const relatives = ["relative1", "relative2", "relative3", "relative4"];
 const App = () => {
   return (
     <div id="main">
-      <ol key={"relativeList"}>
+      <ol id="relativeList" key={"relativeList"}>
         {relatives.map((relative, i) => (
-          <li key={`relativeListItem${i + 1}`}>{relative}</li>
+          <li id={`relativeListItem${i + 1}`} key={`relativeListItem${i + 1}`}>
+            {relative}
+          </li>
         ))}
       </ol>
     </div>
